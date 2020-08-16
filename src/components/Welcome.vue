c825b2b17f2c10718c90b5ebc7223e812104dd1f
KLong <15110232612@163.com>
2020-08-16 13:28:28 +0800

    user over

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>Hello World</h1>
+    <h1>Fuck The World</h1>
   </div>
 </template>
 
@@ -10,5 +10,15 @@ export default {
 </script>
 
 <style scoped>
-
+div{
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  /* align-items: center; */
+}
+h1{
+  font-size: 80px;
+  color: rgb(223, 72, 72);
+  padding-top: 200px;
+}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,29 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Login from "../components/Login.vue";
-import Home from "../components/Home.vue";
-import Welcome from "../components/Welcome.vue";
-import Users from "../components/user/Users.vue";
+// import Login from "../components/Login.vue";
+// import Home from "../components/Home.vue";
+// import Welcome from "../components/Welcome.vue";
+// import Users from "../components/user/Users.vue";
+// import Rights from "../components/power/Rights.vue";
+// import Roles from "../components/power/Roles.vue";
+// import Cate from "../components/goods/Cate.vue";
+// import Params from "../components/goods/Params.vue";
+// import GoodsList from "../components/goods/List.vue";
+// import Report from "../components/report/Report.vue";
+// import Order from "../components/order/Order.vue";
 
+// 路由懒加载
+const Login = () => import("../components/Login.vue")
+const Home = () => import( "../components/Home.vue");
+const Welcome = () => import( "../components/Welcome.vue");
+const Users = () => import( "../components/user/Users.vue");
+const Rights = () => import( "../components/power/Rights.vue");
+const Roles = () => import( "../components/power/Roles.vue");
+const Cate = () => import( "../components/goods/Cate.vue");
+const Params = () => import( "../components/goods/Params.vue");
+const GoodsList = () => import( "../components/goods/List.vue");
+const Report = () => import( "../components/report/Report.vue");
+const Order = () => import( "../components/order/Order.vue");
 Vue.use(VueRouter);
 
 const routes = [
@@ -25,6 +44,30 @@ const routes = [
         path:'/users',
         component: Users,
       },
+      {
+        path:'/rights',
+        component: Rights,
+      },
+      {
+        path:'/roles',
+        component: Roles,
+      },
+      { 
+        path: '/categories',
+        component: Cate 
+      },
+      { path: '/params',
+        component: Params 
+      },
+      { path: '/goods',
+        component: GoodsList 
+      },
+      { path: '/orders',
+        component: Order 
+      },
+      { path: '/reports',
+        component: Report
+      }
     ]
   },
   {
